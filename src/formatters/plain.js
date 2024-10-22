fca60bf2c71cfef2b6008080741efce3d5c9a648
Weloza <danil557@icloud.com>
2024-10-22 14:12:35 +0400

    change formatters/plain.js(func iter)

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -21,17 +21,22 @@ export default function getPlain(tree) {
   function iter(object, path) {
     const result = object.map((key) => {
       const completePath = `${path}${key.key}`;
-      if (key.condition === 'minus') {
-        return `Property '${completePath}' ${data.remove}`;
-      } else if (key.condition === 'plus') {
-        return `Property '${completePath}' ${data.add}${getValue(key.secondValue)}`;
-      } else if (key.condition === 'enclosure') {
-        return iter(key.child, `${completePath}.`);
-      } else if (key.condition === 'different') {
-        return `Property '${completePath}' ${data.update} From ${getValue(key.firstValue)} to ${getValue(key.secondValue)}`;
-      } else {
-        return undefined;
+      let stringWithChanges;
+      switch (key.condition) {
+        case('minus'):
+          stringWithChanges = `Property '${completePath}' ${data.remove}`;
+          break;
+        case('plus'):
+          stringWithChanges = `Property '${completePath}' ${data.add}${getValue(key.secondValue)}`;
+          break;
+        case('enclosure'):
+          stringWithChanges = iter(key.child, `${completePath}.`);
+          break;
+        case('different'):
+          stringWithChanges = `Property '${completePath}' ${data.update} From ${getValue(key.firstValue)} to ${getValue(key.secondValue)}`;
+          break;
       }
+      return stringWithChanges;
     });
     return result.filter((n) => n !== undefined).join('\n');
   }
